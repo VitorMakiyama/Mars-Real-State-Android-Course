5ccfa365fab0211996f1c17c1a85bd9611461864
VitorMakiyama <jv.makiyama@usp.br>
2023-06-08 06:23:10 -0300

    First Commit Udacity course of Android development with Kotlin - Internet access

---
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://mars.udacity.com/"
+
+// Creates a Moshi object with the MoshiBuilder giving it an adapter
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+// Creates a retrofit object, associating it to a converter (our moshi object, created by a MoshiConvertFactory)
+// and the base URL
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    // Using suspend makes this a Coroutine function, automatically integrated in Retrofit and Moshi
+    @GET("realestate")
+    suspend fun getProperties(): List<MarsProperty>
+}
+
+/** Since the retrofit.create() is an expensive call, we expose our API to the rest of the app using
+ * a singleton 'object'
+ */
+object MarsApi {
+    val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
+}
---
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.network
+import com.squareup.moshi.Json
+
+data class MarsProperty(
+    val id: String,
+    // This annotation allows us to change our structured class name while still correctly associating it to the JSON
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    // A double can store any number value of a JSON
+    val price: Double
+)
